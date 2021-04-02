1ddb4265889ee190e0971e06950afc1063c982c6
Nick Shute <nshute@umich.edu>
2021-04-02 13:22:54 -0400

    Create EmailTest.java
    Created the testing file for the Email class.

---
+++ EmailTest.java
@@ -0,0 +1,40 @@
+package org.apache.commons.mail;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.subethamail.smtp.server.Session;
+
+public class EmailTest {
+
+	public static final String[] TEST_EMAILS = { "ab@bc.com", "a.b@c.org", 
+			"abcdefghijklmnopqrst@abcdefghijklmnopqrst.com.bd"};
+	
+	/* Concrete Email class for testing */
+	private EmailConcrete email;
+
+	@Before
+	public void setUpEmailTest() throws Exception {
+		
+		email = new EmailConcrete();
+		
+	}
+	
+	@After
+	public void tearDownEmailTest() throws Exception {
+		
+		
+		
+	}
+}
